55a6cda0d2a902b4b9b6fa8a56347dd08c0564e5
ninab95 <ninaborkovic.95@gmail.com>
2020-10-11 00:17:22 +0200

    Header added

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="nav-links">
+        <li className="home">Home</li>
+        <li>Features</li>
+        <li>Lifestyle</li>
+        <li>Travel</li>
+        <li>Shop</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </div>
+      <div className="nav-icons">
+        <i className="fab fa-facebook-f"></i>
+        <i className="fab fa-twitter"></i>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-pinterest"></i>
+        <i className="fas fa-heart"></i>
+        <i className="fas fa-rss"></i>
+        <i className="fas fa-shopping-cart"></i>
+      </div>
+      <div className="search-bar">
+        <div></div>
+        <input type="text" placeholder="Search and hit enter" />
+        <i className="fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
