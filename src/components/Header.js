49f579586a64ae24c71e0b2e81961587ccab8fcd
ninab95 <ninaborkovic.95@gmail.com>
2020-11-06 19:37:23 +0100

    Making link active

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import "./Header.scss";
 
 function Header() {
+  const location = useLocation();
   const [isMenuOpened, setMenuOpened] = useState(false);
+
+  function isPageActive(page) {
+    console.log(location.pathname);
+    if (location.pathname === page) {
+      return 'active'
+    }
+  }
+
   return (
     <div className="container">
       <div onClick={() => setMenuOpened(true)} className="bars-wrapper">
@@ -11,19 +21,19 @@ function Header() {
           <div className="line-second"></div>
           <div className="line-third"></div>
       </div>
-      <div onClick={() => setMenuOpened(false)} className={`nav-links ${isMenuOpened && "opened"}`}>
-        <Link to="/" style={{ textDecoration: "none" }}>
-        <li className="home">Home</li>
+      <div onClick={() => setMenuOpened(false)} className={`nav-links ${isMenuOpened && "opened"}`} >
+        <Link to="/" style={{ textDecoration: "none" }} >
+          <li className={isPageActive( '/')}  >Home</li>
         </Link>
         <li>Features</li>
         <li>Lifestyle</li>
         <li>Travel</li>
         <li>Shop</li>
-        <Link to="/about" style={{ textDecoration: "none" }}>
-        <li className="about">About me</li>
+        <Link to="/about" style={{ textDecoration: "none" }} >
+          <li className={isPageActive( '/about')} >About me</li>
         </Link>
         <Link to="/contact" style={{ textDecoration: "none"}}>
-        <li>Contact</li>
+          <li className={isPageActive( '/contact')} >Contact</li>
         </Link>
       </div>
       <div className="nav-icons">
